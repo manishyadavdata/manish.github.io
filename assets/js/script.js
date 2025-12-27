e5f2beaf043b98b4ffd560e861db0cf5d4977888
Manish Yadav <45384589+Manish-gituser@users.noreply.github.com>
2025-12-27 07:58:13 +0530

    Remove unused modal variables and event listeners
    Removed modal-related variables and event listeners.

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,9 +18,7 @@ sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); }
 
 // testimonials variables
 const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
-const modalContainer = document.querySelector("[data-modal-container]");
-const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
-const overlay = document.querySelector("[data-overlay]");
+
 
 // modal variable
 const modalImg = document.querySelector("[data-modal-img]");
@@ -49,9 +47,7 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 
 }
 
-// add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
+
 
 
 
